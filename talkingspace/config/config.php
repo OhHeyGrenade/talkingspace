0c83c3fc8c8b6fc00e04160d7419f431fdabc10a
OhHeyGrenade <33704406+OhHeyGrenade@users.noreply.github.com>
2018-08-22 17:34:21 -0400

    Update config.php

--- talkingspace/config/config.php
+++ talkingspace/config/config.php
@@ -8,4 +8,4 @@ define("DB_NAME", "talkingspace");
 define("SITE_TITLE", "Welcome To TalkingSpace!");
 
 //Paths
-define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/talkingspace/');
+define ('BASE_URI', 'http://OhHeyGrenade.github.io/talkingspace/');
